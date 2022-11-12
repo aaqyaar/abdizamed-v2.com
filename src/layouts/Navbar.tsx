b1f49e48894b1bfadcd9a65d85d9fc3f58076dde
abdizamedmo <abdizamedmo@gmail.com>
2022-11-12 22:04:15 +0300

    navbar App is responsive

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,13 +1,14 @@
+"use client";
 // styles
 import styles from "styles/_layouts/Navbar.module.scss";
 // app
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
-
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 type Props = {};
 
 export default function Navbar({}: Props) {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <nav>
       <div className={styles.container}>
@@ -16,8 +17,8 @@ export default function Navbar({}: Props) {
             <h1>Abdi Zamed Mohamed</h1>
           </Link>
         </div>
-        <div className="menu-nav">
-          <ul className={styles.menu}>
+        <div id="desktop-menu-nav">
+          <ul className={styles.nav}>
             <li>
               <Link href="#">Home</Link>
             </li>
@@ -44,6 +45,50 @@ export default function Navbar({}: Props) {
             </li>
           </ul>
         </div>
+        <div className={styles.menu}>
+          {isOpen ? (
+            <AiOutlineClose
+              onClick={() => setIsOpen((prev) => !prev)}
+              className={styles.menuIcon}
+            />
+          ) : (
+            <AiOutlineMenu
+              onClick={() => setIsOpen((prev) => !prev)}
+              className={styles.menuIcon}
+            />
+          )}
+        </div>
+      </div>
+      <div id="mobile-nav">
+        {isOpen && (
+          <ul className={styles.mobileNav}>
+            <li>
+              <Link href="#">Home</Link>
+            </li>
+            <li>
+              <Link href="#about">About</Link>
+            </li>
+            <li>
+              <Link href="#projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="#contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/blogs">Blogs</Link>
+            </li>
+
+            <li>
+              <Link
+                href="/images/abdizamed.svg"
+                download
+                className={styles.resumeBtn}
+              >
+                Resume
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
