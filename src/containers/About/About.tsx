9db7b8b23d457ff188f7eb81b757a3d1a469840c
abdizamedmo <abdizamedmo@gmail.com>
2022-11-13 22:55:17 +0300

    About section started yet not completed

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "styles/About.module.scss";
 
 type Props = {};
 
 export default function About({}: Props) {
-  return <div>About</div>;
+  return (
+    <section id="about">
+      <div className={styles.aboutContainer}>
+        <h1 className={styles.aboutTitle}>About Me</h1>
+        <p className={styles.aboutSubtitle}>
+          A Full Stack Developer with 2+ years of experience in designing and
+          developing user interfaces, testing, debugging, and training staff
+          within modern technologies. Proven ability in optimizing web & mobile
+          functionalities that improve data retrieval and workflow efficiencies.
+        </p>
+      </div>
+    </section>
+  );
 }
--- src/containers/Hero/Hero.tsx
+++ src/containers/Hero/Hero.tsx
@@ -1,36 +1,42 @@
 import Link from "next/link";
 import React from "react";
-import { AiOutlineDownload, AiOutlineInbox } from "react-icons/ai";
+import { AiOutlineDownload, AiOutlineGithub } from "react-icons/ai";
 import styles from "styles/Hero.module.scss";
 
 type Props = {};
 
 export default function Hero({}: Props) {
   return (
-    <div className={styles.heroContainer}>
-      <h1 className={styles.heroTitle}>
-        Hi, {"I'm"} <span className={styles.heroName}>Abdi Zamed</span>
-      </h1>
-      <p className={styles.heroSubtitle}>
-        A Full Stack Developer with 2+ years of experience in designing and{" "}
-        <br />
-        developing user interfaces, testing, debugging, and training staff{" "}
-        <br />
-        within modern technologies. Proven ability in optimizing web & mobile{" "}
-        <br />
-        functionalities that improve data retrieval and workflow efficiencies.
-      </p>
+    <section id="hero">
+      <div className={styles.heroContainer}>
+        <h1 className={styles.heroTitle}>
+          Hi, {"I'm"} <span className={styles.heroName}>Abdi Zamed</span>
+        </h1>
+        <p className={styles.heroSubtitle}>
+          A Full Stack Developer with 2+ years of experience in designing and{" "}
+          <br />
+          developing user interfaces, testing, debugging, and training staff{" "}
+          <br />
+          within modern technologies. Proven ability in optimizing web & mobile{" "}
+          <br />
+          functionalities that improve data retrieval and workflow efficiencies.
+        </p>
 
-      <div id="hero-btn" className={styles.heroBtnContainer}>
-        <Link href="/abdizamed.pdf" className={styles.heroBtn}>
-          <AiOutlineDownload className={styles.heroBtnIcon} />
-          Resume
-        </Link>
-        <Link href="#contact" className={styles.heroBtn}>
-          <AiOutlineInbox className={styles.heroBtnIcon} />
-          Contact
-        </Link>
+        <div id="hero-btn" className={styles.heroBtnContainer}>
+          <Link href="/abdizamed.pdf" className={styles.heroBtn}>
+            <AiOutlineDownload className={styles.heroBtnIcon} />
+            Resume
+          </Link>
+          <Link
+            href="https://github.com/abdi-aaqyaar"
+            target="_blank"
+            className={styles.heroBtn}
+          >
+            <AiOutlineGithub className={styles.heroBtnIcon} />
+            Github
+          </Link>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
